e030984c328aba0a872f0b099dfbb79041003ffa
ollz272 <46482091+ollz272@users.noreply.github.com>
2019-03-11 09:49:05 +0000

    Update LinkedList.h
    added list class

--- LinkedList.h
+++ LinkedList.h
@@ -1,1 +1,20 @@
 
+class List{
+	private: //data
+		
+		typedef struct node{
+			int data; //Creates data stored within the node
+			node* next; //this is a pointer to the next node within the list.
+		}* nodePtr; //nodePtr now means node*.
+		
+		nodePtr head;
+		nodePtr current;
+		nodePtr temp;
+		notePtr tail;
+		
+	public: //functions to manipulate data
+		list(); //constructor
+		void AddNode(int addData);
+		void DeleteNode(int delData);
+		void PrintList();
+};
