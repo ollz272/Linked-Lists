ac2e127a6ad4f083ccb65aaf0bed11ad76d80236
ollz272 <46482091+ollz272@users.noreply.github.com>
2019-03-11 10:53:15 +0000

    Update LinkedList.cpp
    Function definitions

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,1 +1,60 @@
+// Example program
+#include <iostream>
+#include <cstdlib>
+#include "Linked List.h"
+using namespace std;
+
+List::List(){
+	head = NULL;
+	current = NULL;
+	temp = NULL;
+	tail = NULL;
+}
+
+void List::AddNode(int addData){
+	nodePtr n = new node;
+	n-> next = NULL;
+	n-> data = addData;
+	
+	if(head != NULL){
+		current = head;
+		while(current->next !=NULL){
+			current = current -> next;
+		}
+		current-> next = n;
+	}
+	else{
+		head = n;
+	}
+}
+
+void List::DeleteNode(int delData){
+	nodePtr delPtr = NULL;
+	temp = head;
+	curr = head;
+	
+	while(curr !=NULL && curr -> data != delData){
+		temp = curr;
+		curr = curr ->next;
+	}
+	if(curr == NULL){
+		cout << delData << " was not in the list\n";
+	}
+	else{
+		delPtr ==curr;
+		curr = curr-> next;
+		temp -> next = curr;
+		delete delPtr;
+		cout << "The value " << delData << " was deleted\n";
+	}
+}
+
+void List::PrintList(){
+	curr = head;
+	while(curr != NULL){
+		cout<< curr -> data << endl;
+		curr = curr -> next;
+	}
+}
+
 
